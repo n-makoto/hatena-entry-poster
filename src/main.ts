7e3bde76eb8bf332abd13be12cff6a014926e837
nakagawa.makoto <wwwww.desire@gmail.com>
2020-09-21 03:30:12 +0900

    embbed hatena-blog-api

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import * as core from '@actions/core'
+import {postEntry} from './postEntry'
 import {wait} from './wait'
 
 async function run(): Promise<void> {
@@ -6,9 +7,25 @@ async function run(): Promise<void> {
     const ms: string = core.getInput('milliseconds')
     core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
 
+    const API_KEY: string = core.getInput('API_KEY')
+    const BLOG_ID: string = core.getInput('BLOG_ID')
+    const HATENA_ID: string = core.getInput('HATENA_ID')
+
     core.debug(new Date().toTimeString())
     await wait(parseInt(ms, 10))
     core.debug(new Date().toTimeString())
+
+    const title = 'test title.'
+
+    const content = [
+      '[link](http://example.com)',
+      '',
+      '- item 1',
+      '- item 2',
+      '- item 3'
+    ].join('\n')
+
+    await postEntry({API_KEY, BLOG_ID, HATENA_ID, title, content})
 
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
---
+++ src/postEntry.ts
@@ -0,0 +1,33 @@
+import {Client} from 'hatena-blog-api'
+
+const postEntry = async ({
+  API_KEY,
+  BLOG_ID,
+  HATENA_ID,
+  title,
+  content
+}: {
+  API_KEY: string
+  BLOG_ID: string
+  HATENA_ID: string
+  title: string
+  content: string
+}): Promise<void> => {
+  const client = new Client({
+    apiKey: API_KEY,
+    authType: 'basic',
+    blogId: BLOG_ID,
+    hatenaId: HATENA_ID
+  })
+  await client.list()
+
+  await client.create({
+    categories: ['category1'],
+    content,
+    contentType: 'text/x-markdown',
+    draft: true,
+    title
+  })
+}
+
+export {postEntry}
